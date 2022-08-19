782356635b12241a29e0a27ed2a8a187eef40256
joaquim-22 <joaquimfsilva22@gmail.com>
2022-08-19 14:57:38 +0200

    fix password

--- SERVER/config/db.js
+++ SERVER/config/db.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://joaquim:joaquim22@cluster0.ercv5yu.mongodb.net/test",
+    "mongodb+srv://joaquim:"+process.env.DB_PASSWORD+"@cluster0.ercv5yu.mongodb.net/test",
   )
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Failed to connect to MongoDB", err));
